772792ebce0c390b434325b79f985651c8681289
thiago-santanna <uzmaki_thiago@outlook.com>
2022-07-30 17:51:02 -0300

    tela de acesso finlaizada

--- src/components/LoginOrRegister/index.tsx
+++ src/components/LoginOrRegister/index.tsx
@@ -4,12 +4,34 @@ export function LoginOrRegister() {
   return (
     <>
       <div className={styles.wrapper}>
+        <div>Informe seu e-mail e código de acesso.</div>
         <form className={styles.wrapperForm}>
-          <span>form</span>
-          <div className={styles.wrapperInput}>
-            <input type="text" name="email" id="email" />
-          </div>
+          <input
+            className={styles.Input}
+            placeholder="Digite seu e-mail"
+            type="text"
+            name="email"
+            id="email"
+          />
+          <input
+            className={styles.Input}
+            placeholder="Digite seu código de acesso"
+            type="password"
+            name="senha"
+            id="senha"
+          />
         </form>
+        <div>Ou inscreva-se solicitando seu código de acesso</div>
+        <form className={styles.wrapperFormCreate}>
+          <input
+            className={styles.Input}
+            placeholder="Digite seu e-mail"
+            type="text"
+            name="email"
+            id="email"
+          />
+        </form>
+        <button className={styles.button}>E lá vamos nós</button>
       </div>
     </>
   );
